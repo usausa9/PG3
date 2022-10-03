8dd01487b9316bf5dcda5d99a14fa7dd15ff296e
Wsd kazu <grazie.taiko@gmail.com>
2022-10-03 11:33:21 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,12 +1,36 @@
 #include <stdio.h>
 
-int Recursive(int n) {
-	
+int NormalWage(int wage, int hour) {
+
+	return wage * hour;
+}
+
+int RecursiveWage(int wage, int hour) {
+
+	if (hour <= 0)
+	{
+		return 0;
+	}
+
+	return wage + RecursiveWage(wage * 2 - 50, hour - 1);
 }
 
 int main(void) {
 
-	int normalWage = 1072;
+	int workHours = 8;
+
+	printf("一般的な賃金体系 : %d円\n", NormalWage(1072, workHours));
+	printf("再帰的な賃金体系 : %d円\n", RecursiveWage(100, workHours));
+
+
+	if (NormalWage(1072, workHours) >= RecursiveWage(100, workHours))
+	{
+		printf("よって一般的な賃金体系の方が賃金が多い。\n");
+	}
+	else
+	{
+		printf("よって再帰的な賃金体系の方が賃金が多い。\n");
+	}
 
 	return 0;
 }
