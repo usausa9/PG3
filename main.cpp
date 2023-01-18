3b3ed510aced5460f443ad7701a6e7df60b05afe
usausa9 <k021g1269@g.neec.ac.jp>
2023-01-18 14:57:05 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,6 +1,61 @@
-#include <stdio.h>
+﻿#include <stdio.h>
+#include <iostream>
+
+#include <list>
+using namespace std;
 
 int main(void) {
 	
+	list<const char*> list
+	{
+		"Tokyo",
+		"Kanda",
+		"Akihabara",
+		"Okachimachi",
+		"Ueno",
+		"Uguisudani",
+		"Nippori",
+		"Tabata",
+		"Komagome",
+		"Sugamo",
+		"Otsuka",
+		"Ikebukuro",
+		"Mejiro",
+		"Takadanobaba",
+		"Shin - Okubo",
+		"Shinjuku",
+		"Yoyogi",
+		"Harajuku",
+		"Shibuya",
+		"Ebisu",
+		"Meguro",
+		"Gotanda",
+		"Osaki",
+		"Shinagawa",
+		"Tamachi",
+		"Hamamatsucho",
+		"Shimbashi",
+		"Yurakucho"
+	};
+
+	cout << "1970年" << "\n";
+	for (auto itr = list.begin(); itr != list.end(); itr++) {
+		cout << *itr << "\n";
+	}
+	// 西日暮里を挿入する
+	list.insert(next(list.begin(), 7), "Nishi - Nippori");
+
+	cout << "\n2019年" << "\n";
+	for (auto itr = list.begin(); itr != list.end(); itr++) {
+		cout << *itr << "\n";
+	}
+	// 高輪ゲートウェイを挿入する
+	list.insert(next(list.begin(), 25), "Takanawa - Gateway");
+	cout << "\n2022年" << "\n";
+
+	for (auto itr = list.begin(); itr != list.end(); itr++) {
+		cout << *itr << "\n";
+	}
+
 	return 0;
 }
